eef5dd2e6de3193e3dff953ad29f7fc306a8181a
akkuvy <67875631+akkuvy@users.noreply.github.com>
2021-01-26 12:59:06 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
     db: null,
   };
   module.exports.connect = function (done) {
-    const url ='mongodb://localhost:27017' 
+    const url ='mongodb+srv://vyshnavk:YlbCWaMYfV2VVSKo@cluster0.qmvxs.mongodb.net/Gogaga?retryWrites=true&w=majority' 
     const dbname = "Gogaga";
   
     mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
